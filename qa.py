730b1dacd98fa45abb69c4df6290ef7f0a58b00a
Rahul Sundar <rahul.sundar95@gmail.com>
2023-10-25 08:32:57 +0530

    Update qa.py

--- qa.py
+++ qa.py
@@ -30,11 +30,16 @@ from urllib.parse import urlparse
 # -------DATA FRAME/DOCX/TEXT HANDLING----------$
 import pandas as pd
 import pprint as pp
-from docx import Document
+from docx import Document as Docxreader
 from docx.shared import Inches
 import textwrap
 import glob
 
+import fitz
+from unidecode import unidecode
+import pandas as pd
+import numpy as np
+import re
 # -----------------------------------------------------------------------------------------#
 # ---------------------------------OPENAI and LANGCHAIN DEPENDENCIES-----------------------#
 # -----------------------------------------------------------------------------------------#
@@ -62,7 +67,54 @@ from langchain.document_loaders import TextLoader
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 
-
+from langchain.llms import OpenAI
+from langchain import PromptTemplate, LLMChain
+import random
+import time
+from langchain.chains import RetrievalQA
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.vectorstores import Chroma
+from langchain.llms import GPT4All, LlamaCpp
+from langchain.document_loaders import TextLoader, PDFMinerLoader, CSVLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.docstore.document import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.docstore.document import Document
+from langchain import HuggingFacePipeline
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from tqdm import tqdm
+from langchain.vectorstores import FAISS
+from langchain.document_loaders import (
+    CSVLoader,
+    EverNoteLoader,
+    PDFMinerLoader,
+    TextLoader,
+    UnstructuredEmailLoader,
+    UnstructuredEPubLoader,
+    UnstructuredHTMLLoader,
+    UnstructuredMarkdownLoader,
+    UnstructuredODTLoader,
+    UnstructuredPowerPointLoader,
+    UnstructuredWordDocumentLoader,
+)
+import os
+import glob
+import argparse
+import openai
+from time import sleep
+chunk_size = 512 #512
+chunk_overlap = 50
+#from dotenv import load_dotenv
+from langchain.chains import RetrievalQA
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.vectorstores import Chroma
+from langchain.llms import GPT4All, LlamaCpp
+from langchain.docstore.document import Document
 # --------------KEYBERT MODEL ---------------#
 from keybert import KeyBERT
 
@@ -80,7 +132,7 @@ def readdoc_splittext(filename):
     This functions takes in an input document and finds the headings, and
     splits them based on the chunks needed.
     """
-    document = Document(filename)
+    document = Docxreader(filename)
     headings = []
     para_texts = []
     i = 0
@@ -119,7 +171,7 @@ def readdoc_splittext(filename):
     for text in para_texts:
         all_text += text
 
-    a = glob.glob("*.docx")
+    a = glob.glob(filename)
 
     chunk_size = 1024
     chunk_overlap = 10
@@ -157,6 +209,143 @@ def remove_newlines(serie):
     serie = serie.replace("  ", " ")
     return serie
 
+def get_block_dict_fromDoc(doc):
+    '''
+    Takes in the document raw data and extracts the document text and meta data into a single dictionary.
+    '''
+    block_dict = {}
+    page_num = 1
+    for page in doc: # Iterate all pages in the document
+        file_dict = page.get_text('dict') # Get the page dictionary
+        block = file_dict['blocks'] # Get the block information
+        block_dict[page_num] = block # Store in block dictionary
+        page_num += 1 # Increase the page value by 1
+    return block_dict
+
+
+def get_docfeature_dataframe(block_dict):
+    """
+    takes in the block dict and then gives out a Pandas data frame with the most important document features.
+    Once the font_size column is populated, and the index of the rows are available, one can simply order them better based on
+    the font size, and document tree (sections - sub-sections - sub-sub-sections, etc)
+    """
+    spans = pd.DataFrame(columns=['xmin', 'ymin', 'xmax', 'ymax', 'text', 'tag'])
+    rows = []
+    for page_num, blocks in block_dict.items():
+        for block in blocks:
+            if block['type'] == 0:
+                for line in block['lines']:
+                    for span in line['spans']:
+                        xmin, ymin, xmax, ymax = list(span['bbox'])
+                        font_size = span['size']
+                        text = unidecode(span['text'])
+                        span_font = span['font']
+                        #is_upper = False
+                        is_bold = False
+                        if "bold" in span_font.lower():
+                            is_bold = True
+                        #if font_size >= 12.0:
+                        #    is_upper = True
+                        if text.replace(" ","") !=  "":
+                            rows.append((xmin, ymin, xmax, ymax, text, is_bold, span_font, font_size))
+    span_df = pd.DataFrame(rows, columns=['xmin','ymin','xmax','ymax', 'text','is_bold','span_font', 'font_size'])
+    return span_df
+
+
+def get_paragraphs(headings, paragraph_sentences):
+
+    """
+    Takes the headings and the sentences extracted from the document data frame.
+    Unifies the sentences under the section headings as a single paragraph.
+    """
+    paragraph_list = len(headings.index)*[""]
+    print(paragraph_list)
+    print(len(headings.index))
+
+    if len(headings.index) > 1:
+        print("Length is more than 1")
+        for i in range(len(headings.index)):
+
+            paragraph = paragraph_list[i]
+            print(len(paragraph))
+            if i < len(headings.index) - 1:
+                print(headings.index[i], headings.index[i+1])
+                for j in range(headings.index[i]+1,headings.index[i+1]):
+                    if j in paragraph_sentences.index:
+                        paragraph += paragraph_sentences.loc[j]
+                paragraph_list[i] = paragraph
+
+            elif i == len(headings.index) - 1:
+
+                for j in range(headings.index[i]+1,paragraph_sentences.index[-1]):
+                    if j in paragraph_sentences.index:
+                        paragraph += paragraph_sentences.loc[j]
+                paragraph_list[i] = paragraph
+            print(i)
+
+        return  paragraph_list
+
+    elif len(headings.index) == 1:
+        print("Length is 1")
+        paragraph = paragraph_list[0]
+        print(len(paragraph))
+
+        for j in range(headings.index[0]+1, paragraph_sentences.index[-1]):
+            if j in paragraph_sentences.index:
+                paragraph += paragraph_sentences.loc[j]
+        paragraph_list[0] = paragraph
+        
+
+        return  paragraph_list
+
+def readdoc_splittext_pdf(filename):
+    """
+    This functions takes in an input document in pdf form and finds the headings, and
+    splits them based on the chunks needed. 
+    """
+    doc = fitz.open(filename)
+    block_dict = get_block_dict_fromDoc(doc)
+    span_df = get_docfeature_dataframe(block_dict)
+    doc_clean = span_df[span_df["font_size"]>=span_df["font_size"].mode()[0]]
+    #doc_clean.head()
+    paragraphs = doc_clean.text[doc_clean.font_size == span_df.font_size.mode()[0]]
+    print(paragraphs.values,paragraphs.index)
+    headings = doc_clean.text[doc_clean.font_size > span_df.font_size.mode()[0]]
+    print(headings.values, headings.index)
+    paragraph_list = get_paragraphs(headings, paragraphs)
+    headings_list = headings.values.tolist()
+
+    n = 1500 #Number of characters to be included in a single chunk of text  
+    
+    all_text=''
+    for text in paragraph_list:
+        all_text+=text
+
+    
+
+    a=glob.glob(filename)
+    print(a)
+    chunk_size = 1024
+    chunk_overlap=10
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    texts_isb=[]
+    texts_raw = []
+    documents=[]
+    for i in range(len(a)):
+
+        documents.extend(PDFMinerLoader(a[i]).load())
+        for j in range(len(text_splitter.split_documents(PDFMinerLoader(a[i]).load()) ) ):
+            text_chunk=text_splitter.split_documents(PDFMinerLoader(a[i]).load())[j]
+            text_chunk.page_content = text_chunk.page_content.replace('\n', ' ')
+            text_chunk.page_content = text_chunk.page_content.replace('\\n', ' ')
+            text_chunk.page_content = text_chunk.page_content.replace('  ', ' ')
+            text_chunk.page_content = text_chunk.page_content.replace('  ', ' ')
+            print(text_chunk)
+            
+            texts_isb.append(text_chunk.page_content)
+            texts_raw.append(text_chunk)
+    text_split=texts_isb
+    return all_text, text_split, texts_raw, headings_list, paragraph_list
 
 # ----------------CREATE CONTEXT-----------------------#
 def create_context(query, text_split, headings, para_texts):
@@ -179,7 +368,7 @@ def create_context(query, text_split, headings, para_texts):
     keywords = []
 
     for j in range(len(keywords_query)):
-        if keywords_query[j][1] > 0.35:
+        if keywords_query[j][1] > 0.3:
             if keywords_query[j][0] not in keywords_q:
                 # print(keywords[j][0], keywords[j][1])
                 keywords_q.append(keywords_query[j][0])
@@ -205,7 +394,7 @@ def create_context(query, text_split, headings, para_texts):
                             # Add the heading of  the para corresponding to the sentence
                             keyword_doc_sent.append(h)
 
-                if keywords[j][1] > 0.35:  # and keywords[j][0] not in keyword_doc ):
+                if keywords[j][1] > 0.3:  # and keywords[j][0] not in keyword_doc ):
                     if keywords[j][0] not in keyword_doc_sent:
                         keyword_doc_sent.append(keywords[j][0])
         keyword_doc[i] = keyword_doc_sent
@@ -239,7 +428,8 @@ def create_context(query, text_split, headings, para_texts):
 
 
 # ------------------------SLIM KAR BASED CHATBOT----------------------------#
-def chatbot_slim(query, text_split, headings, para_texts):
+# ------------------------SLIM KAR BASED CHATBOT----------------------------#
+def chatbot_slim(query, context, keywords):#text_split, headings, para_texts):
     """
     Here, this function takes in the textual query, along with the textual context and uses KAR framework to geerate a suitable response
     with little to almost no hallucinations. Here, openai's davnci-003 has been used to generate the response.
@@ -248,7 +438,7 @@ def chatbot_slim(query, text_split, headings, para_texts):
     if input:
         stime = time.time()
 
-        context, keywords = create_context(query, text_split, headings, para_texts)
+        #context, keywords = create_context(query, text_split, headings, para_texts)
 
         ctype = ["stuff", "map_reduce", "refine", "map_rerank"]
         template = """
@@ -297,8 +487,8 @@ def chatbot_slim(query, text_split, headings, para_texts):
         chain_type_kwargs = {"prompt": PROMPT}
 
         question = query
-
-        openai.api_key = SECRET_TOKEN
+        context = context
+        openai.api_key = "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"
         model = "text-davinci-003"
         chat = openai.Completion.create(
             prompt=f"You answer question based on context below, and if the question can't be answered based on the context, say \"I don't know\"\n\nContext: {context}\n\n---\n\nQuestion: {question}\nAnswer:",
@@ -316,6 +506,44 @@ def chatbot_slim(query, text_split, headings, para_texts):
         ].strip()  # ['choices'][0]['message']['content']
 
         return reply, context, keywords
+
+def create_db(text_chunk):
+
+    hf= OpenAIEmbeddings(model="text-embedding-ada-002", openai_api_key=openai.api_key)
+    db = FAISS.from_documents(text_chunk, hf)
+    db.save_local("faiss_index_anupam")
+    db=FAISS.load_local("faiss_index_anupam", hf)
+    return hf, db
+    
+def chatbot(question, db):
+
+    openai.api_key = "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"
+    
+    ctype=['stuff', 'map_reduce', 'refine', 'map_rerank']
+
+    retriever = db.as_retriever(search_type='similarity', search_kwargs={"k": 5} )#do not increase k beyond 3, else
+    docs_and_scores = db.similarity_search_with_score(question)
+
+    for doc, score in  docs_and_scores:
+        print(f"Metadata: {doc.metadata}, Score: {score}")
+        
+    
+    llm = OpenAI(model='text-davinci-003',temperature=0, openai_api_key=openai.api_key)
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type=ctype[0], retriever=retriever, return_source_documents=True)
+
+    
+    query = question
+    
+    res = qa(query)
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    
+    messages=[
+        {"role": "system", "content": "You are a friendly chatbot who answers questions asked based only on the context provided, if you dont know the answer say I do not know"},
+        {"role": "user", "content": f"{res}"}
+    ])
+    answer= response["choices"][0]["message"]["content"]
+    return answer
 
 
 # ----------------TEXT TO SPEECH FUNCTION FOR ANSWER READOUT---------#
