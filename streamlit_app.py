338e13051fc9de5b85d46bacc8cbd6fc6fe9adce
Anupam <92312729+amaze18@users.noreply.github.com>
2023-10-26 18:31:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -246,11 +246,11 @@ if not audio.empty():
             #)
             # if "messages" not in st.session_state.keys():
             #    st.session_state.messages = [{"role": "assistant", "content": query}]
-        with st.chat_message("assistant"):
-            st.write("If I heard you write, your question is as follows ")
-        with st.chat_message("user"):
-            st.write(query)
-        query_status = 1
+    with st.chat_message("assistant"):
+        st.write("If I heard you write, your question is as follows ")
+    with st.chat_message("user"):
+        st.write(query)
+    query_status = 1
 
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [
