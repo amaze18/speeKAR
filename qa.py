2aa029866e1cb8e2269da7616cf4e68cb22b6d57
Rahul Sundar <rahul.sundar95@gmail.com>
2023-11-01 23:36:18 +0530

    Update qa.py

--- qa.py
+++ qa.py
@@ -520,7 +520,7 @@ def chatbot_slim(query, context, keywords):#text_split, headings, para_texts):
 
         return reply, context, keywords
 
-@st.cache_resource(show_spinner=False)
+@st.cache_resource(show_spinner=True)
 def create_db(_text_chunk):
 
     hf= OpenAIEmbeddings(model="text-embedding-ada-002", openai_api_key=openai.api_key)
