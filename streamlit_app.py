e4160be28e45e35652f814e6dbb9ff6b47703b1b
Rahul Sundar <rahul.sundar95@gmail.com>
2023-11-02 10:56:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -187,6 +187,7 @@ elif uploaded_file is None:
     st.session_state["text_input_status"] = False
     st.session_state["query_status"] = False
     st.session_state["audio_input_status"] = False
+    st.cache_data.clear()
     
 if (uploaded_file is not None): # and (st.session_state["uploaded_status"] == True): #and (st.session_state["db_created"] == False) and (st.session_state["query_counter"]==0):
     # To read file as bytes:
