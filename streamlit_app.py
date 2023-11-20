10d50fcb34577e66ce7575688027291cce00d572
Rahul Sundar <rahul.sundar95@gmail.com>
2023-11-20 23:31:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -218,10 +218,11 @@ if (uploaded_file is not None) and (st.session_state["uploaded_status"] == True)
     elif (".doc" in filename) and (".docx" not in filename): #uploaded_file.name:
         with st.chat_message("assistant"):
             st.write("Currently support is only for *.docx and *.pdf format documents only. Please convert your document into one of these formats!")
-            st.session_state["uploaded_status"] = False
-            st.rerun()
+        #st.session_state["uploaded_status"] = False
+        #st.rerun()
         #all_text, text_split, text_chunk, headings, para_texts = readdoc_splittext(filename)#uploaded_file.name)
         # print(text_split)
+        break
     elif ".pdf" in filename: #uploaded_file.name:
         all_text, text_split, text_chunk, headings, para_texts = readdoc_splittext_pdf(filename)#uploaded_file.name)
         # print(text_split)
