1c31dbca5af1ff62f53927b033a489a4a9d0a190
Rahul Sundar <rahul.sundar95@gmail.com>
2023-11-21 20:48:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -418,24 +418,14 @@ if (uploaded_file is not None): # and (st.session_state["uploaded_status"] == Tr
 # -------------------------QUERY AUDIO INPUT - RETURNING TEXT QUERY-------------#
 # ------------------------------------------------------------------------------#
 
-#if "messages" not in st.session_state.keys():
-#    st.session_state.messages = [
-#        {"role": "assistant", "content": "Ask anything from the document!"}
-#    ]
     
-#if (uploaded_file is not None) and (st.session_state["db_created"] == True) and (st.session_state["query_status"] == False):
+
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask anything from the document!"}
+    ]
 
 
-# Generate a new response if last message is not from assistant
-#if st.session_state.messages[-1]["role"] != "assistant":
-#    with st.chat_message("assistant"):
-#        with st.spinner("Thinking..."):
-#            pass
-#        pass
-#    message = {"role": "assistant", "content": ans}
-#    st.session_state.messages.append(message)
-
-# Display chat messages
 for message in st.session_state.messages[::-1]:
     with st.chat_message(message["role"]):
         st.write(message["content"])
