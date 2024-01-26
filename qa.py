46bb3a165c8c775a3afe9d05e114073aae948b0a
Saikrishna-Bulusu <142135673+Saikrishna-Bulusu@users.noreply.github.com>
2024-01-26 23:12:17 +0530

    qa.py

--- qa.py
+++ qa.py
@@ -123,7 +123,7 @@ from keybert import KeyBERT
 # ----------DEFAULTS:------------#
 LANGUAGE = "en"
 
-SECRET_TOKEN = os.environ["SECRET_TOKEN"]
+SECRET_TOKEN = "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"
 openai.api_key = SECRET_TOKEN
 
 
