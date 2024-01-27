9eb328eea63ee1914611668491d55a01ce157137
Saikrishna-Bulusu <142135673+Saikrishna-Bulusu@users.noreply.github.com>
2024-01-27 22:47:58 +0530

    qa.py

--- qa.py
+++ qa.py
@@ -555,7 +555,7 @@ def chatbot(question, db):
     docs_and_scores = db.similarity_search_with_score(question)
         
     
-    llm = OpenAI(model='text-davinci-003',temperature=0, openai_api_key=openai.api_key)
+    llm = OpenAI(model='gpt-3.5-turbo-instruct',temperature=0, openai_api_key=openai.api_key)
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type=ctype[0], retriever=retriever, return_source_documents=True)
 
     
@@ -563,7 +563,7 @@ def chatbot(question, db):
     
     res = qa(query)
     response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
+    model="gpt-3.5-turbo-instruct",
     
     messages=[
         {"role": "system", 
